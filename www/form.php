74943cd3614b2bcd098782366ec051f70aefa62b
Nikvs84 <nikvs84@gmail.com>
2017-01-16 12:45:28 +0500

    Add forms processing

---
+++ www/form.php
@@ -0,0 +1,6 @@
+	<form name="my_form" action="script.php?id=15" method="post">
+		<div>Имя: <input type="text" name="firstname"/></div>
+		<div>Email: <input type="text" name="email"/></div>
+		Сообщение:<div><textarea cols="40" rows="10" name="message"/></textarea></div>
+		<div><input type="submit" name="send"></div>
+	</form>
---
+++ www/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Site.local :-)</title>
+		<meta http-equiv="content-type" content="text/html charset=utf-8"/>
+		<link rel="stylesheet" type="text/css" href="styles/style.css"/>
+	</head>
+	<body>
